ae60e08a0442c03d8cff7e7b21d0c530ef5db800
Anushka J <79107280+anushkajlr@users.noreply.github.com>
2023-11-27 10:40:19 +0530

    printing out dimensions

--- apps/animation.py
+++ apps/animation.py
@@ -19,9 +19,16 @@ device = torch.device(f"cuda:{args.gpu}")
 
 econ_dict = torch.load(f"./results/econ/cache/{args.name}/econ.pt")
 smplx_pkl = np.load(f"./examples/motions/{args.motion}.pkl", allow_pickle=True)
+print("looking a structure of smplx_pkl")
+for k,v in smplx_pkl:
+    print(k,v.shape)
 smplx_pose_mat = torch.tensor(smplx_pkl['pred_thetas'])
+print("taking a look as shape of theta parameters")
+print(smplx_pose_mat.shape)
 smplx_transl = smplx_pkl['transl']
 smplx_pose = rotation_matrix_to_angle_axis(smplx_pose_mat.view(-1, 3, 3)).view(-1, 55, 3)
+print("taking a look at shape of smplx_pose")
+print(smplx_pose.shape)
 smplx_pose[:, 23:23 + 2] *= 0.0    # remove the pose of eyes
 
 n_start = 0
